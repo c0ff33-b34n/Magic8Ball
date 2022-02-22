555ad3f77cb371e7d0062409411e5efc2b0778a6
Stuart Bowes <stuartbowes2000@gmail.com>
2022-02-22 14:53:09 +0000

    Generate a random number from 1 to 5 and display the corresponding image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -31,10 +32,19 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset('images/ball1.png'),
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+            print('Ball Number $ballNumber');
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
     );
   }
 }
